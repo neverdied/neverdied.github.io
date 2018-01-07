00468d05683ef654835caea7dc44bbad8d97e0ac
neverdied <31559600+I-am-bad-boy@users.noreply.github.com>
2018-01-08 00:47:40 +0800

    20180108

--- assets/countdown/jquery.countdown.js
+++ assets/countdown/jquery.countdown.js
@@ -1,10 +1,3 @@
-/**
- * @name		jQuery Countdown Plugin
- * @author		Martin Angelov
- * @version 	1.0
- * @url			http://tutorialzine.com/2011/12/countdown-jquery/
- * @license		MIT License
- */
 
 (function($){
 	
